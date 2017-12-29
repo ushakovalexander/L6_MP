33fc491fb0439dd8f7c6485b8d1974676e5685f4
Alexander Ushakov <ushakov@renatus.com>
2017-12-29 10:03:24 +0200

    Cleanup

--- Assets/Scripts/ToggleButtonController.cs
+++ Assets/Scripts/ToggleButtonController.cs
@@ -3,26 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ToggleButtonController : MonoBehaviour {
-	public bool isPrimary = true;
+  public bool isPrimary = true;
 
-	public GameObject primaryIcon;
-	public GameObject secondaryIcon;
+  public GameObject primaryIcon;
+  public GameObject secondaryIcon;
 
-	void Start() {
-		UpdateState(isPrimary);
-	}
+  void Start() {
+    UpdateState(isPrimary);
+  }
 
-	public void ToggleState() {
-		UpdateState(!isPrimary);
-	}
+  public void ToggleState() {
+    UpdateState(!isPrimary);
+  }
 
-	public void UpdateState(bool isPrimary) {
-		this.isPrimary = isPrimary;
-		if(primaryIcon != null) {
-			primaryIcon.SetActive(isPrimary);
-		}
-		if(secondaryIcon != null) {
-			secondaryIcon.SetActive(!isPrimary);
-		}
-	}
+  public void UpdateState(bool isPrimary) {
+    this.isPrimary = isPrimary;
+    if(primaryIcon != null) {
+      primaryIcon.SetActive(isPrimary);
+    }
+    if(secondaryIcon != null) {
+      secondaryIcon.SetActive(!isPrimary);
+    }
+  }
 }
